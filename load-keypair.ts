6d9f2deee3b1f887713560f2a27d1efd70ce6c9f
9ightshade <aniekemeusanga@gmail.com>
2024-11-24 23:44:03 +0100

    generate and load keypair

---
+++ load-keypair.ts
@@ -0,0 +1,9 @@
+//LOAD EXISTING KEYPAIR FORM SECRET KEY
+import "dotenv/config";
+import { getKeypairFromEnvironment } from "@solana-developers/helpers";
+
+const keypair = getKeypairFromEnvironment("SECRET_KEY");
+
+console.log(
+    `✅ Finished! We've loaded our secret key securely, using an env file!`,
+  );
